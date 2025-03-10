af00bfa1702725ba4e71700dd77c7d2d0ecfc824
Nikita Shishkov <atos_wwww@mail.ru>
2025-03-10 21:30:46 +0300

    dobavil logiku dlya chteniya puti k faylu

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int x = new Scanner(System.in) .nextInt();
-        System.out.println("Введите второе число:");
-        int y = new Scanner(System.in) .nextInt();
-        System.out.println("Сумма чисел:"+" "+(x+y));
-        System.out.println("Разность чисел:"+" "+(x-y));
-        System.out.println("Произведение чисел:"+" "+(x*y));
-        double w = (double)x/y;
-        System.out.println("Частное чисел:"+" "+w);
+        int res =0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path); //существует ли файл путь к которому указан?
+            boolean fileExists = file.exists(); // -//- тру или фолс по вопросу выше
+            boolean isDirectory = file.isDirectory(); // является ли указанный путь путём именно к файлу, а не к папке?
+            res=res+1;
+            if (fileExists==true && isDirectory==false) {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер" + " " + res);
+            }
+            if (fileExists == false) System.out.println("Файл не существует");
+            if (isDirectory == true) System.out.println("Указан путь к папке, а не к файлу");
+            }
+
+        }
     }
-}
+
